ef77f39d98dc6d66ded223200842fadccd127535
Palani <Govindharaju.Palani@emc.com>
2019-04-24 19:45:46 +0530

    Wed 04/24/2019 19:45:46.51

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -4,6 +4,9 @@ set -e
 
 app_guid=`cf app $1 --guid`
 echo " appguid : $app_guid "
+
+echo `jq`
+
 credentials=`cf curl /v2/apps/$app_guid/env | jq '.system_env_json.VCAP_SERVICES | if .["p-mysql"] != null then .["p-mysql"] else .["p.mysql"] end | .[0].credentials'`
 
 echo "printing credentials... "
