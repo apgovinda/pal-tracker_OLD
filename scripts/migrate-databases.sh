1573a9db1d16b23fd0815ffbc2f643ccd0af2638
Palani <Govindharaju.Palani@emc.com>
2019-04-24 21:43:32 +0530

    Wed 04/24/2019 21:43:32.47

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -10,7 +10,7 @@ credentials=`cf curl /v2/apps/$app_guid/env | jq '.system_env_json.VCAP_SERVICES
 
 echo "printing credentials... "
 echo "credentials : $credentials "
-echo "dolor 1  : $1 "
+
 
 
 ip_address=`echo $credentials | jq -r '.hostname'`
